9715d9e50fe116f8a0438b440a99cbd953b86b84
chdefrene <defrenechr@me.com>
2014-03-07 15:21:30 +0100

    Bug fixes

--- src/com/proj/database/Database.java
+++ src/com/proj/database/Database.java
@@ -2,10 +2,13 @@ package com.proj.database;
 
 /**
  * Created with IntelliJ IDEA.
- * User: Alfredo
+ * User: Teh baws
  * Date: 06.03.14
  * Time: 14:44
  * To change this template use File | Settings | File Templates.
  */
 public class Database {
+	
+	
+	
 }
