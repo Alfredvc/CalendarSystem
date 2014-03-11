d7255f7d5d88c1f7351eebee791e5edc3e3ddad8
Alexander Olofsson <Alexolo1993@hotmail.com>
2014-03-11 14:01:21 +0100

    fixed login so client.java runs the login frame

--- src/com/proj/exec/Client.java
+++ src/com/proj/exec/Client.java
@@ -1,11 +1,13 @@
 package com.proj.exec;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Alfredo
- * Date: 07.03.14
- * Time: 12:48
- * To change this template use File | Settings | File Templates.
- */
-public class Client {
+import com.proj.gui.Login;
+
+public class Client extends Login{
+	
+	public static void main(String args[]) {
+		
+		new Login();
+	
+		
+	}
 }
--- src/com/proj/gui/Login.java
+++ src/com/proj/gui/Login.java
@@ -2,28 +2,40 @@ package com.proj.gui;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 
 
-public class Login extends JPanel{
+public class Login extends JFrame{
 
+    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize(); // Dimensions of client screen
+    
+    private JTextField emailInput;
+	private JPasswordField passwordInput;
 
-    static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	private JTextField emailInput;
-	private JTextField passwordInput;
-
+	
+	
 	public Login() {
 		
+		
+		// Setting up the Frame, setting the size, position and making it fixed size
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(420, 170);
+		int xLocation = (int) (screenSize.getWidth() - getWidth()) / 2;
+		int yLocation = (int) (screenSize.getHeight() - getHeight()) / 2;		
+		setLocation(xLocation, yLocation);
+		setResizable(false);
 		setLayout(null);
 		
+		// Adding the different labels
 		JLabel emailLabel = new JLabel("Email:", SwingConstants.RIGHT);
 		emailLabel.setBounds(10, 20, 80, 25);
 		add(emailLabel);
@@ -33,36 +45,38 @@ public class Login extends JPanel{
 		JLabel passwordLabel = new JLabel("Password:", SwingConstants.RIGHT);
 		passwordLabel.setBounds(10, 50, 80, 25);
 		add(passwordLabel);
+		
+		// Adding Username/Email and Password fields
 		emailInput = new JTextField();
 		emailInput.setColumns(20);
 		emailInput.setBounds(100, 20, 160, 25);;
 		add(emailInput);
-		passwordInput = new JPasswordField();
+		passwordInput = new JPasswordField(); // NB JPasswordField, use getPassword()
 		passwordInput.setBounds(100, 50, 260, 25);
 		add(passwordInput);
 
+		// Creating both the login and cancel buttons
 		JButton loginButton = new JButton("Login");
 		loginButton.setBounds(100, 80, 100, 30);
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//System.out.println(emailInput.getText() + "." + new String(passwordInput.getPassword()));
+			}
+		});	
 		add(loginButton);
 		
-		JButton registerButton = new JButton("Cancel");
-		registerButton.setBounds(260, 80, 100, 30);
-		add(registerButton);
-	}
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.setBounds(260, 80, 100, 30);
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+				dispose(); // Closes the window and hopefully stops it all
+			}
+		});	
+		add(cancelButton);
 
-	public static void main(String args[]) {
-
-		JFrame frame = new JFrame("Calender System - Log In");
-		frame.setSize(420, 170);
-		int xLocation = (int) (screenSize.getWidth() - frame.getWidth()) / 2;
-		int yLocation = (int) (screenSize.getHeight() - frame.getHeight()) / 2;		
-		frame.setLocation(xLocation, yLocation);
-		frame.setResizable(false);
-		frame.add(new Login());
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+		setVisible(true);
 	}
-	
-	
 }
+
