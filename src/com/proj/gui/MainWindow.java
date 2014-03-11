f900c76fbcb9a028a355136933071a9248e40bc9
chdefrene <defrenechr@me.com>
2014-03-11 14:58:19 +0100

    Added button

--- src/com/proj/gui/MainWindow.java
+++ src/com/proj/gui/MainWindow.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class MainWindow extends JFrame {
 
@@ -35,8 +37,15 @@ public class MainWindow extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnDontPressThe = new JButton("Don't press the button!");
+		btnDontPressThe.setBounds(126, 124, 198, 29);
+		contentPane.add(btnDontPressThe);
+		
+		JLabel lblButton = new JLabel("Button:");
+		lblButton.setBounds(196, 80, 61, 16);
+		contentPane.add(lblButton);
 	}
-
 }
