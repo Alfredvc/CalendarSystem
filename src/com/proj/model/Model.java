f3b0be8a57460002b29d6953257290c3ed6668cb
eivindfosse <eivindfosse@192.168.38.100>
2014-03-13 00:44:40 +0100

    Har gjort ferdig generatorklassen og testet den. Endret getParticipants i appointment.

--- src/com/proj/model/Model.java
+++ src/com/proj/model/Model.java
@@ -2,6 +2,7 @@ package com.proj.model;
 
 import java.util.ArrayList;
 import java.sql.Date;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.UUID;
@@ -15,13 +16,15 @@ public class Model {
 	
 	
 	
-	public void deleteAppointment(UUID id){                //her staar det int i klassediagrammet, men det er vel UUID vi gikk for??
+	public void deleteAppointment(UUID id){                
 		this.appointments.remove(id);
 	}
 	
 
 
-	public void addAppointment(Appointment app){this.appointments.put(app.getId(), app);} // Naar en appointment addes, skal den først addes i modellaget for den gaar til databasen??
+	public void addAppointment(Appointment app){
+		this.appointments.put(app.getId(), app);
+	} 
 
 
 	
@@ -74,13 +77,11 @@ public class Model {
 	}
 	
 	public ArrayList<MeetingRoom> getFreeMeetingRooms(Date startTime, Date endTime){
-		ArrayList<MeetingRoom> freeRooms= new ArrayList();
-		for(MeetingRoom m: this.getMeetingRooms()){
-			freeRooms.add(m);
-		}             
+		ArrayList<MeetingRoom> freeRooms= new ArrayList<>(Arrays.asList(this.getMeetingRooms()));
 		for(UUID key: appointments.keySet()){
 			Appointment app=appointments.get(key);
-			if((app.getStartTime().before(endTime) && app.getEndTime().after(endTime)) || (app.getStartTime().before(startTime) && app.getEndTime().after(startTime))){
+			if((app.getStartTime().before(endTime) && app.getEndTime().after(endTime)) ||
+					(app.getStartTime().before(startTime) && app.getEndTime().after(startTime))){
 				
 				freeRooms.remove(app.getMeetingRoom());
 			};
