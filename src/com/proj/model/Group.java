acc3d3fdafb102ee5dc8c1e235b6b991036f530e
Truls Rustad Fossum <trulfos@gmail.com>
2014-03-12 11:15:30 +0100

    Added missing methods and constructors to model
    Created some needed methods for the database layer in the model layer, mainly
    constructors, getters and setters.
    
    Conflicts:
    	src/com/proj/model/Employee.java

--- src/com/proj/model/Group.java
+++ src/com/proj/model/Group.java
@@ -1,13 +1,24 @@
 package com.proj.model;
 
+import java.util.ArrayList;
+
 public class Group {
-	private Employee[] employees;
+	private final String name;
+	private ArrayList<Employee> employees = new ArrayList<>();
 	
-	public Group(Employee[] employees) {
-		this.employees = employees;
+	public Group(String name) {
+		this.name = name;
 	}
 	
 	public Employee[] getEmployees() {
-		return employees;
+		return (Employee[]) employees.toArray(new Employee[employees.size()]);
+	}
+	
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+	}
+	
+	public String getName() {
+		return name;
 	}
 }
--- src/com/proj/model/MeetingRoom.java
+++ src/com/proj/model/MeetingRoom.java
@@ -1,19 +1,36 @@
 package com.proj.model;
 
 public class MeetingRoom {
-	private String roomNr;
+	private String roomNr,
+			name,
+			notes;
+	
 	private int capacity;
 	
 	public MeetingRoom(String roomNr, int capacity) {
 		this.roomNr = roomNr;
 		this.capacity = capacity;
 	}
-	
+
+	public MeetingRoom(String roomNr, int capacity, String name, String notes) {
+		this(roomNr, capacity);
+		this.name = name;
+		this.notes = notes;
+	}
+
 	public String getRoomNr() {
 		return roomNr;
 	}
-	
+
 	public int getCapacity() {
 		return capacity;
+	}
+	
+	public String getNotes() {
+		return notes;
+	}
+	
+	public String getName() {
+		return name;
 	}
 }
