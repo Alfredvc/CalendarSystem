f0aac358cdde87e5a1137cbfc318fe0e66027919
morten <morten@78.91.80.40>
2014-03-18 12:39:03 +0100

    Ganske Ferdig

--- src/com/proj/gui/NotificationViewPanel.java
+++ src/com/proj/gui/NotificationViewPanel.java
@@ -1,9 +1,13 @@
 package com.proj.gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -11,7 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
+import javax.swing.Scrollable;
 
 import com.proj.model.Notification;
 
@@ -21,23 +25,27 @@ public class NotificationViewPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	NotificationScrollPane scrollPane;
-	JButton closeButton;
+	JButton closeButton, addButton;
 	ArrayList<Notification> notifications = new ArrayList<>();
-	GridBagConstraints x = new GridBagConstraints();
-	
+	GridBagConstraints c = new GridBagConstraints();
 	
 
 	public NotificationViewPanel() {
 		setLayout(new GridBagLayout());
 		scrollPane = new NotificationScrollPane();
-		x.gridx=0;
-		x.gridy=0;
-		add(scrollPane,x);
+		c.gridx = 1;
+		c.gridy = 0;
+		add(scrollPane, c);
 		closeButton = new JButton("CLose");
-		x.gridx=1;
-		x.gridy=0;
-		add(closeButton,x);
+		c.gridx = 2;
+		c.gridy = 1;
+		c.anchor = GridBagConstraints.LAST_LINE_END;
+		add(closeButton, c);
 		closeButton.addActionListener(new CloseButtonListener());
+		addButton = new JButton("Add");
+		add(addButton);
+		addButton.addActionListener(new CloseButtonListener());
+		
 
 	}
 
@@ -48,17 +56,21 @@ public class NotificationViewPanel extends JPanel {
 			if (e.getSource() == closeButton) {
 				System.exit(ABORT);
 				System.out.println("Closing");
+				
+			}
+			if(e.getSource()==addButton){
+				notifications.add(new Notification("adding new notification"));
 			}
 
 		}
 	}
 
 	public static void main(String[] args) {
-	
+
 		NotificationViewPanel viewPanel = new NotificationViewPanel();
 		JFrame frame = new JFrame("Notifications");
 		frame.getContentPane().add(viewPanel);
-		frame.setResizable(true);
+		frame.setResizable(false);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
@@ -70,17 +82,21 @@ public class NotificationViewPanel extends JPanel {
 		/**
 		 * 
 		 */
-		
+
+		GridBagConstraints x = new GridBagConstraints();
 		private static final long serialVersionUID = 1L;
 
 		public NotificationScrollPane() {
-
-			
-			setSize(500, 500);
-			notifications.add(new Notification("Your are invited to fløtepotetefhjebf"));
-			notifications.add(new Notification("Your are invited to fløjebf"));
-			notifications.add(new Notification("Your are invited to fløteadsadbf"));
-			notifications.add(new Notification("Your are invited to fløtepotefhjebf"));
+			JPanel panel = new JPanel();
+			panel.setLayout(new GridBagLayout());
+			notifications
+					.add(new Notification(
+							"Your are invited to fløtepoteter"));
+			notifications.add(new Notification("Your are invited to fløjebf afdhakjfhjdsfjdkfhdaskjfh"));
+			notifications.add(new Notification(
+					"Your are invited to fløteadsadbf"));
+			notifications.add(new Notification(
+					"Your are invited to fløtepotefhjebf"));
 			notifications.add(new Notification("what the fuck??!!!"));
 			notifications.add(new Notification("what fuck??!!!"));
 			notifications.add(new Notification("hmm??!!!"));
@@ -90,16 +106,29 @@ public class NotificationViewPanel extends JPanel {
 			notifications.add(new Notification("Is it working yet?"));
 			notifications.add(new Notification("Is it working yet?"));
 			notifications.add(new Notification("Is it working yet?"));
+			notifications.add(new Notification("Message received?"));
 			notifications.add(new Notification("Is it working yet?"));
+			notifications.add(new Notification("Is it working yet?"));
+			notifications.add(new Notification("It's working!!!"));
+			
 			for (int i = 0; i < notifications.size(); i++) {
 				ButtonPane buttonPane = new ButtonPane(notifications.get(i));
-				add(buttonPane);
-				
-		}
+				x.gridx = 0;
+				x.gridy = i;
+				x.ipady=50;
+				x.anchor = GridBagConstraints.LAST_LINE_END;
+				panel.add(buttonPane, x);
+
+			}
 			setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+			setViewportView(panel);
+			//viewport.setPreferredSize(getPreferredScrollableViewportSize());
+			viewport.setPreferredSize(new Dimension(700,300));
 			
-			
+
 		}
+
+		
 
 	}
 
@@ -111,38 +140,42 @@ public class NotificationViewPanel extends JPanel {
 		JLabel label;
 		JButton viewAppointmentsButton;
 		String notificationString;
-		GridBagConstraints c = new GridBagConstraints();
-
+		GridBagConstraints x = new GridBagConstraints();
+		
+		public String convertToMultiline(String orig)
+		{
+		    return "<html>" + orig.replaceAll("\n", "<br>  ");
+		}
+		
 		public ButtonPane(Notification notification) {
+			
+			
+			
 			setLayout(new GridBagLayout());
-		
-		
-				
-				notificationString = notification.getText() + " "
-						+ "notification.getTimestamp()";
-				label = new JLabel(notificationString);
-				c.gridx = 0;
-				c.gridy = 0;
-				add(label, c);
-				viewAppointmentsButton = new JButton("View Appointment");
-				c.gridx = 1;
-				c.gridy = 0;
-				add(viewAppointmentsButton, c);
-				viewAppointmentsButton.addActionListener(new ActionListener() {
+			notificationString = notification.getText() + "\n "
+					+ "notification.getTimestamp()" + "               ";
+			label = new JLabel(convertToMultiline(notificationString), JLabel.LEFT);
+//			x.anchor = GridBagConstraints.LINE_END;
+	//		label.setText(convertToMultiline(notificationString));
+			label.setPreferredSize(new Dimension(400,50));
+			add(label);
+			
+			viewAppointmentsButton = new JButton("View Appointment");
+			add(viewAppointmentsButton);
+			viewAppointmentsButton.addActionListener(new ActionListener() {
 
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						System.out.println("hmmmmmm");
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					System.out.println(notificationString);
 
-						// TODO Auto-generated method stub
+					// TODO Auto-generated method stub
 
-					}
-				});
-
-			}
+				}
+			});
+//			setBackground(Color.RED);
 
 		}
 
 	}
 
-
+}
