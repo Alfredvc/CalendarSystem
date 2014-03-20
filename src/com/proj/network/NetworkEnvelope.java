f9fd77b7baaddbba3d2f87e9fb0d65dc29ec6ab2
Alfredvc <erpa_1913@hotmail.com>
2014-03-20 17:55:35 +0100

    new protocol

---
+++ src/com/proj/network/NetworkEnvelope.java
@@ -0,0 +1,11 @@
+package com.proj.network;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alfredo
+ * Date: 20.03.14
+ * Time: 17:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class NetworkEnvelope {
+}
